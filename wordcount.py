3a37db83b2d25a32864517fe503e9c7f5dc1ff27
dante0527 <19dante.j.lee@gmail.com>
2021-11-26 18:57:05 -0500

    fixed tims program

--- wordcount.py
+++ wordcount.py
@@ -9,25 +9,13 @@ with open("beatles.txt", "r") as file:
     Replace3 = Replace2.replace('"', " ")
     Replace4 = Replace3.replace(", ", " ")
     Replace5 = Replace4.replace("\)", " ")
-<<<<<<< HEAD
     Replace6 = Replace5.replace("\(", " ")
     Replace7 = Replace6.strip()
     Replace8 = Replace7.split(" ")
 
-# test
-# test(tr)
-for x in Replace8:
-    Replace8.count(x)
-    wordcount.append(Replace8.count(x))
-    print(x, Replace8.count(x))
-=======
-    Replace6 = Replace5.strip()
-    Replace7 = Replace6.split(" ")
-   
 #test
 #test(tr)
 for x in (Replace7):
     Replace7.count(x)
     wordcount.append(Replace7.count(x))
     print(x,Replace7.count(x))
->>>>>>> refs/remotes/origin/main
