abff55f6b28d089c7025d6dc07109e3e0e98b48f
TRudenko22 <timothy.rudenkom@gmail.com>
2021-11-26 16:48:39 -0500

    added array of special special characters

--- wordcount.py
+++ wordcount.py
@@ -1,19 +1,21 @@
 wordcount = []
-#wordcount = {i:wordcount.count(i) for i in wordcount}
-with open("beatles.txt","r") as file:
+filters = [". ", ", ", "\(", "\)"]
+# wordcount = {i:wordcount.count(i) for i in wordcount}
+with open("beatles.txt", "r") as file:
     words = file.read()
+    Replace0 = words.replace(".", ".")
     Replace = words.replace(". ", " ")
     Replace2 = Replace.replace(", ", " ")
-    Replace3 = Replace2.replace("\"", " ")
+    Replace3 = Replace2.replace('"', " ")
     Replace4 = Replace3.replace(", ", " ")
     Replace5 = Replace4.replace("\)", " ")
     Replace6 = Replace5.replace("\(", " ")
     Replace7 = Replace6.strip()
     Replace8 = Replace7.split(" ")
-   
-#test
-#test(tr)
-for x in (Replace8):
+
+# test
+# test(tr)
+for x in Replace8:
     Replace8.count(x)
     wordcount.append(Replace8.count(x))
-    print(x,Replace8.count(x))
+    print(x, Replace8.count(x))
