07dd00a1fd7ea59ac85690ede7b28954712c526b
dante0527 <19dante.j.lee@gmail.com>
2021-11-26 14:23:45 -0500

    Added project files

---
+++ wordcount.py
@@ -0,0 +1,19 @@
+wordcount = []
+#wordcount = {i:wordcount.count(i) for i in wordcount}
+with open("beatles.txt","r") as file:
+    words = file.read()
+    Replace = words.replace(". ", " ")
+    Replace2 = Replace.replace(", ", " ")
+    Replace3 = Replace2.replace("\"", " ")
+    Replace4 = Replace3.replace(", ", " ")
+    Replace5 = Replace4.replace("\)", " ")
+    Replace6 = Replace5.replace("\(", " ")
+    Replace7 = Replace6.strip()
+    Replace8 = Replace7.split(" ")
+   
+#test
+    
+for x in (Replace8):
+    Replace8.count(x)
+    wordcount.append(Replace8.count(x))
+    print(x,Replace8.count(x))
