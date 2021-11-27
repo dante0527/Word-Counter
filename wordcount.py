e7e1042a6bec274a3704a0feab8ea5ba61806898
TRudenko22 <timothy.rudenkom@gmail.com>
2021-11-27 18:48:58 -0500

    regex majick on the words

--- wordcount.py
+++ wordcount.py
@@ -5,21 +5,10 @@ filters = [". ", ", ", "\(", "\)"]
 # wordcount = {i:wordcount.count(i) for i in wordcount}
 with open("beatles.txt", "r") as file:
     words = file.read()
-    Replace0 = words.replace(".", ".")
-    Replace = words.replace(". ", " ")
-    Replace2 = Replace.replace(", ", " ")
-    Replace3 = Replace2.replace('"', " ")
-    Replace4 = Replace3.replace(", ", " ")
-    Replace5 = Replace4.replace("\)", " ")
-    Replace6 = Replace5.replace("\(", " ")
-    Replace7 = Replace6.strip()
-    Replace8 = Replace7.split(" ")
-
-#test
-#test(tr)
-
-noPunc = list(map(lambda x: re.sub("[^\w\s]", '', x), Replace8))
-noNewline = list(map(lambda x: x.replace('\n', ' '), noPunc))
-
+    Replace = words.split()
+    noPunc = list(map(lambda x: re.sub("[^\w\s]", "", x), Replace))
+    noPunc2 = list(map(lambda x: re.sub(r"[\xa0]", "", x), noPunc))
+    noNewline = list(map(lambda x: x.replace("\n", " "), noPunc2))
 
 print(noNewline)
+print(len(noNewline))
