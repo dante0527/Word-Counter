ff8f3be7ac1ea858ce72298fe2123005df2230d7
Tim <timothy.rudenkom@gmail.com>
2021-11-27 22:21:28 -0500

    removed redundancy

--- wordcount.py
+++ wordcount.py
@@ -3,7 +3,7 @@ import re
 with open("beatles.txt", "r") as file:
     words = file.read()
     Replace = words.split()
-    noPunc = list(map(lambda x: re.sub("[^\w\s]", "", x), Replace))
+    noPunc = list(map(lambda x: re.sub("[^\w]", "", x), Replace))
     noPunc2 = list(map(lambda x: re.sub(r"[\xa0]", "", x), noPunc))
     noNewline = list(map(lambda x: x.replace("\n", " "), noPunc2))
 
