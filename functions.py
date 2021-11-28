408cdce512a41dcf14feaae4e45b391d899834fd
dante0527 <19dante.j.lee@gmail.com>
2021-11-27 23:35:11 -0500

    Created function WordCount

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
-def WordCount(rawtextfile):
+import re
 
-	words = file.read()
-    Replace = words.split()
-    noPunc = list(map(lambda x: re.sub("[^\w]", "", x), Replace))
-    noPunc2 = list(map(lambda x: re.sub(r"[\xa0]", "", x), noPunc))
-    noNewline = list(map(lambda x: x.replace("\n", " "), noPunc2))
+def WordCount(rawtextfile):
+    with open(rawtextfile, "r"):
+        words = rawtextfile.read()
+        Replace = words.split()
+        noPunc = list(map(lambda x: re.sub("[^\w]", "", x), Replace))
+        noPunc2 = list(map(lambda x: re.sub(r"[\xa0]", "", x), noPunc))
+        noNewline = list(map(lambda x: x.replace("\n", " "), noPunc2))
+    return noNewline
--- wordcount.py
+++ wordcount.py
@@ -1,11 +1,7 @@
 import re
+from functions import *
 
-with open("beatles.txt", "r") as file:
-    words = file.read()
-    Replace = words.split()
-    noPunc = list(map(lambda x: re.sub("[^\w]", "", x), Replace))
-    noPunc2 = list(map(lambda x: re.sub(r"[\xa0]", "", x), noPunc))
-    noNewline = list(map(lambda x: x.replace("\n", " "), noPunc2))
+WordCount()
 
 print(noNewline)
 print(len(noNewline))
